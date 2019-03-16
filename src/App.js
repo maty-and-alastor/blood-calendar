35bebf01bf9fedcc2370826c6c2e24a44e22b9a6
maty-dragon <maty@projectdrgn.com>
2019-03-16 18:40:35 +0100

    All done

--- src/App.js
+++ src/App.js
@@ -1,28 +1,61 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+// @ts-check
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import * as kv from "idb-keyval";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+const startDates = [
+  [new Date("2019-03-05"), "red"],
+  [new Date("2019-03-06"), "blue"],
+  [new Date("2019-03-09"), "blue"]
+];
+function isSameDay(d1 /*: DateTime*/, d2) {
+  return (
+    d1.getDate() === d2.getDate() &&
+    d1.getMonth() === d2.getMonth() &&
+    d1.getFullYear() === d2.getFullYear()
+  );
+}
+function getDateColor(allDates, date) {
+  const founded = allDates.find(x => isSameDay(x[0], date));
+  return founded ? founded[1] : "default";
 }
 
-export default App;
+function LoadFromIdb() {
+  const [allDates, updateDates] = useState(undefined);
+  function addToDates(date) {
+    updateDates(x => [...x, [date, "red"]]);
+  }
+  useEffect(() => {
+    if (allDates) kv.set("allDates", allDates);
+  }, [allDates]);
+  useEffect(() => {
+    kv.get("allDates").then(allDates => {
+      updateDates(allDates || []);
+    });
+  }, []);
+  return allDates ? (
+    <App {...{ allDates, addToDates }} />
+  ) : (
+    <div style={{ textAlign: "center" }}> Loading</div>
+  );
+}
+
+function App({ allDates, addToDates }) {
+  const [selectedDate, changeSelectedDay] = useState(new Date());
+
+  return (
+    <div className="App">
+      <DatePicker
+        inline
+        selected={selectedDate}
+        onChange={e => changeSelectedDay(e)}
+        dayClassName={curentDate => getDateColor(allDates, curentDate)}
+      />
+      <button onClick={e => addToDates(selectedDate)}>I am bleeding</button>
+    </div>
+  );
+}
+
+export default LoadFromIdb;
